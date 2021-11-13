b1a118ee75de3ed03670b1f57a984886626f4f0b
chiyu-git <873705939@qq.com>
2021-11-13 15:12:30 +0800

    feat: 去除冗余的脚本
    changelog:

--- scripts/constant.ts
+++ scripts/constant.ts
@@ -1,4 +1,2 @@
 export const HOST = 'http://localhost:3000';
-// /user/liyu0109/monthly
-export const DAILY_SEIYUU_SITE = 'https://socialblade.com/twitter/user/liyu0109/monthly';
 export const WEEKLY_SEIYUU_SITE = 'https://headline.client.jp/ranking_f.html';
